a7cca002eb91cc53d832ed76b2f6c7e874013cc1
Oab00 <oab93@hotmail.com>
2015-08-05 19:42:37 +0100

    adding collection route and masonry directive

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,22 +1,62 @@
 'use strict';
 
 angular.module('pinterestNewApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', function ($scope, $http, Auth, $timeout) {
+    $scope.loggedIn = Auth.isLoggedIn();
+    $timeout(function() { $scope.loggedIn = Auth.isLoggedIn(); }, 50);
+    var user = Auth.getCurrentUser();
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    $scope.usernames = [];
+    $scope.filterSelected = false;
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
+    var allPictures = [];
+    $scope.pictures = [];
+
+    $scope.getPictures = function() {
+      $http.get('/api/pictures').success(function(pictures) {
+        $scope.pictures = pictures;
+        allPictures = pictures;
+
+        var temp = pictures.map(function(picture) {
+          return picture.user.name;
+        }).forEach(function(tmp) {
+          if ($scope.usernames.indexOf(tmp) === -1) {
+            $scope.usernames.push(tmp);
+          }
+        });
+      });
+    };
+    $scope.getPictures();
+
+    $scope.addPicture = function() {
+      if($scope.newPicture === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+      $http.post('/api/pictures', { 
+        url: $scope.newPicture,
+        user: user
+      }).success(function(picture) {
+          $scope.pictures.push(picture);
+          $scope.newPicture = '';
+
+          if ($scope.usernames.indexOf(picture.user.name) === -1) {
+            $scope.usernames.push(picture.user.name);
+          }
+      });
     };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+    $scope.filterSelect = function(index) {
+      $scope.pictures = allPictures;
+      
+      $scope.filterSelected = $scope.usernames[index];
+      $scope.pictures = $scope.pictures.filter(function(picture) {
+        return picture.user.name === $scope.usernames[index];
+      }); 
     };
+
+    $scope.clearFilter = function() {
+      $scope.pictures = allPictures;
+      $scope.filterSelected = false;
+    };
+
   });
